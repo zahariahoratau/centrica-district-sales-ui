fc151ef7fb2bd098ac8cb3945b7f4c1b6e9ffbfd
Zack <zahariahoratau@gmail.com>
2023-11-20 02:14:18 +0100

    Created initial MainView and MainViewModel.

---
+++ DistrictSales.UI.Presentation/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Caliburn.Micro;
+using DistrictSales.UI.Presentation.ViewModels.Districts;
+using DistrictSales.UI.Presentation.ViewModels.Salespeople;
+using DistrictSales.UI.Presentation.Views.Districts;
+using DistrictSales.UI.Presentation.Views.Salespeople;
+
+namespace DistrictSales.UI.Presentation.ViewModels;
+
+public sealed class MainViewModel : Conductor<object>
+{
+    private readonly IWindowManager _windowManager;
+
+    public MainViewModel(IWindowManager windowManager)
+    {
+        DisplayName = "District Sales";
+        _windowManager = windowManager;
+    }
+
+    public void LoadDistrictsPage()
+    {
+        var districtsViewModel = new DistrictsMenuViewModel(_windowManager);
+        var districtsView = new DistrictsMenuView
+        {
+            DataContext = districtsViewModel
+        };
+        districtsView.Show();
+    }
+
+    public void LoadSalespeoplePage()
+    {
+        var salespeopleViewModel = new SalespeopleMenuViewModel(_windowManager);
+        var salespeopleView = new SalespeopleMenuView
+        {
+            DataContext = salespeopleViewModel
+        };
+        salespeopleView.Show();
+    }
+}
---
+++ DistrictSales.UI.Presentation/Views/MainView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace DistrictSales.UI.Presentation.Views;
+
+public partial class MainView : Window
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
