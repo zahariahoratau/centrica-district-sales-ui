913d9ee1e01e9ecfe548579accc962ccf85754a5
Zack <zahariahoratau@gmail.com>
2023-11-20 02:10:45 +0100

    Created Domain layer classes:
    - District
    - Salesperson

---
+++ DistrictSales.UI.Domain/Models/District.cs
@@ -0,0 +1,12 @@
+﻿namespace DistrictSales.UI.Domain.Models;
+
+public class District
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedAtUtc { get; set; }
+    public bool IsActive { get; set; }
+    public short? NumberOfStores { get; set; }
+    public Salesperson PrimarySalesperson { get; set; }
+    public List<Salesperson> SecondarySalespeople { get; set; }
+}
---
+++ DistrictSales.UI.Domain/Models/Salesperson.cs
@@ -0,0 +1,12 @@
+﻿namespace DistrictSales.UI.Domain.Models;
+
+public class Salesperson
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateOnly BirthDate { get; set; }
+    public DateOnly HireDate { get; set; }
+    public string Email { get; set; }
+    public string? PhoneNumber { get; set; }
+}
