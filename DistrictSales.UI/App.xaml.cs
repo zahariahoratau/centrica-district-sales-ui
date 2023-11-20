64c11a440fcc6f0f8db34c079546def98313f46e
Zack <zahariahoratau@gmail.com>
2023-11-20 02:10:22 +0100

    Created UI project files for running WPF project.

---
+++ DistrictSales.UI/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using DistrictSales.UI.Presentation;
+
+namespace DistrictSales.UI;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        // Configure Caliburn.Micro bootstrapper
+        var bootstrapper = new Bootstrapper();
+
+        // Continue with the default WPF startup
+        base.OnStartup(e);
+    }
+}
